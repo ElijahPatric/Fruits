938752247b2f2d6414f0f32f5beddef9394f9283
Elijah Patric <foreverelijah@gmail.com>
2022-10-27 16:34:46 -0500

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Elijah on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
